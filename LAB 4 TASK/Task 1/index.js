55563f01e452f5483ad9d95df5ad2b195b2c6f56
Qillu21 <103613353+Qillu21@users.noreply.github.com>
2022-04-21 23:56:43 +0800

    Add files via upload

---
+++ LAB 4 TASK/Task 1/index.js
@@ -0,0 +1,16 @@
+$(function(){
+    var button = $("#button_change");
+
+
+    $(button).click(function() {
+
+        var inputValue = $("#input_color").val();
+   
+
+        var element = $(".cow_color");
+     
+     
+        $(element).html(inputValue);
+    });
+    
+})
